9a2880a423612d69a6fcba271f7da9d1e934e845
Jaime Burbano <burbanojaime98@gmail.com>
2020-05-29 22:58:26 -0500

    changes

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const url = 'https://api.coincap.io/v2'
 
 function getAssets() {
-  return fetch(`${url}/assets?limit=50`)
+  return fetch(`${url}/assets?limit=20`)
     .then(res => res.json())
     .then(res => res.data)
 }
